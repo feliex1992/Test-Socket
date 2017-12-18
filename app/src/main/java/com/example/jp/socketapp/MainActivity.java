a305645a48f1bc03c8bf792181697af2f63e4662
Jafar Pahrudin <jafar.pahrudin@gmail.com>
2017-12-18 22:26:41 +0700

    day01.003
    Berhasil connect ke server socket visual basic.

--- app/src/main/java/com/example/jp/socketapp/MainActivity.java
+++ app/src/main/java/com/example/jp/socketapp/MainActivity.java
@@ -2,12 +2,21 @@ package com.example.jp.socketapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import java.security.Permission;
 
 public class MainActivity extends AppCompatActivity {
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void Connect(View view)
+    {
         TCPClient tcpClient = new TCPClient();
         tcpClient.Connect("192.168.2.11",1024);
     }
--- app/src/main/java/com/example/jp/socketapp/TCPClient.java
+++ app/src/main/java/com/example/jp/socketapp/TCPClient.java
@@ -14,7 +14,7 @@ public class TCPClient {
     private static String TAG = "TCPClient";
     private String serverIP = "192.168.2.11";
     private long startTime = 01;
-    private int serverPort = 1234;
+    private int serverPort = 1024;
     private Socket connectionSocket;
 
 public class ConnectRunnable implements Runnable
@@ -22,18 +22,23 @@ public class ConnectRunnable implements Runnable
     public void run(){
         try {
 
-            Log.d(TAG,"C: Connecting...");
+            Log.d(TAG, "C: Connecting...");
 
             InetAddress serverAddr = InetAddress.getByName(serverIP);
             startTime = System.currentTimeMillis();
-
+            //Create a new instance of Socket
             connectionSocket = new Socket();
-            connectionSocket.connect(new InetSocketAddress(serverAddr,serverPort),5000);
+
+            //Start connecting to the server with 5000ms timeout
+            //This will block the thread until a connection is established
+            Log.d(TAG, "Server IP : " + serverIP);
+            Log.d(TAG, "Server Port : " + serverPort);
+            connectionSocket.connect(new InetSocketAddress(serverAddr, serverPort), 5000);
 
             long time = System.currentTimeMillis() - startTime;
-            Log.d(TAG, "Connected! Current duration : " + time + " ms");
+            Log.d(TAG, "Connected! Current duration: " + time + "ms");
         } catch (Exception e){
-
+            Log.d(TAG,"Error "+ e.toString());
         }
         Log.d(TAG,"Connection Thread Stopped.");
     }
@@ -42,6 +47,7 @@ public void Connect(String ip, int port)
 {
     serverIP = ip;
     serverPort = port;
-    new Thread(new TCPClient.ConnectRunnable()).start();
+
+    new Thread(new ConnectRunnable()).start();
 }
 }
